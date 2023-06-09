492dc2982cae3f8a98f9f9e2e7b124a9bcd1d6aa
Tsuchiya-Ringtech <tsuchiya.kazunori@ring-tech.jp>
2023-06-09 10:34:17 +0900

    Initial Commit

---
+++ ScreenTransition/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ScreenTransition
+//
+//  Created by Kazunori Tsuchiya on 2023/06/09.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
